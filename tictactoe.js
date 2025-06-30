86dbbdee445b3b25955f2561bd6044b9545e9cca
Abhiraj Sharma <iamabhirajsharma@gmail.com>
2025-07-01 03:42:05 +0530

    new

---
+++ tictactoe.js
@@ -0,0 +1,85 @@
+let boxes=document.querySelectorAll(".inbox");
+let winnerbox=document.querySelector("#winner");
+let ngmbtn=document.querySelector("a");
+let msgbox=document.querySelector(".msgbox");
+let button=document.querySelector("#button");
+let curr="X";
+let winer;
+let pstostrt=document.querySelector("#pstostrt");
+let restart=document.querySelector("#restart");
+
+pstostrt.addEventListener("click",()=>{
+    pstostrt.setAttribute("class","msgbox");
+    restart.classList.remove("msgbox");
+    boxes.forEach((box)=>{
+        box.classList.remove("msgbox");
+    })
+})
+
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(curr=="O"){
+            box.innerHTML="O";
+            winer="O"
+            curr="X";
+        }
+        else{
+            box.innerHTML="X";
+            winer="X";
+            curr="O";
+        }
+        box.classList.add("disable");
+        if(winner()){
+            winsteps();
+        }
+    })
+})
+
+
+const winsteps=()=>{
+    msgbox.setAttribute("id","msgbox2")
+    winnerbox.innerHTML=`Congratulations! Player ${winer} is the winner`;
+    ngmbtn.innerHTML="NEW GAME";
+    ngmbtn.setAttribute("id","newgame");
+    button.setAttribute("id","restart2");
+    disable();
+}
+
+// const disable= ()=>{
+//     for(let box of boxes){
+//         box.classList.add("disable");    
+//     }
+// }
+const disable=()=>{
+        boxes.forEach((box)=>{
+        box.classList.add("disable");
+    })
+}
+    
+
+let checking=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
+const winner=()=>{
+    for(let check of checking){
+        let a=boxes[check[0]].innerHTML;
+        let b=boxes[check[1]].innerHTML;
+        let c=boxes[check[2]].innerHTML;
+        if(a=="X"&&b=="X"&&c=="X"||a=="O"&&b=="O"&&c=="O"){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
